917af5342eedbb60ca34a6a1bdf28f45dfe9c82b
谭浪 <2735546870@qq.com>
2025-11-27 15:14:59 +0800

    Initial Next.js project upload

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+    ],
+    formats: ['image/webp', 'image/avif'],
+  },
+  experimental: {
+    optimizePackageImports: ['@heroicons/react'],
+  },
 };
 
 export default nextConfig;
